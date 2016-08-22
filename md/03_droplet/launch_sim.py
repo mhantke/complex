d4aa38734c0a74668589c2cd61a594bb57d4084f
hantke@xray.bmc.uu.se <hantke@xray.bmc.uu.se>
2016-08-22 11:28:56 +0200

    after work session with Erik on Friday. We prepare the rubber string simulation in a droplet - not done yet.

--- md/03_droplet/launch_sim.py
+++ md/03_droplet/launch_sim.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
    parser.add_argument('-o', '--output-folder', type=str, help="output folder", nargs='?', required=True)
    parser.add_argument('-s', '--start-at', type=int, help="start at given processing step")
    parser.add_argument('-i', '--single-step', type=int, help="run only single step")
+   parser.add_argument('-m', '--mode', type=str, help="simulation mode", default="pull")
    if(len(sys.argv) == 1):
       parser.print_help()
       sys.exit(0)
@@ -39,28 +40,32 @@ if __name__ == "__main__":
              "#SBATCH --workdir=%s\n" % output_folder] 
 
    exs = ["00_init", 
-          "01_trjconv",
-          "02_editconf",
-          "03_solvate",
-          "04_cut_water_sphere",
-          "05_ions",
-          "06_genion",
-          "07_em_pre",
-          "08_em.sh",
-          "09_em_post",
-          "10_nvt_pre",
-          "11_nvt.sh",
-          "12_nvt_post",
-          "13_npt_pre",
-          "14_npt.sh",
-          "15_sim_pre",
-          "16_sim.sh",
+          "01_box",
+          "02_solvate",
+          "03_sphere",
+          "04_genion_pre",
+          "05_genion",
+          "06_em_pre",
+          "07_em",
+          "08_nvt_pre",
+          "09_nvt",
+          "10_sim_pre",
+          "11_sim_pull",
    ]
+
+
    links = exs + ["env", "input", "itp", "mdp", "env", "scripts"]
  
-
    for link in links:
       lines += ["ln -s -f ../%s\n" % link]
+
+   allowed_modes = ["pull"]
+   if args.mode not in allowed_modes:
+      print "ERROR: Invalid mode: %s" args.mode
+      return
+
+   if args.mode == "pull":
+      do("ln -s sim_pull.mdp mdp/sim.mdp")
 
    if args.start_at is not None:
       exs = [ex for ex in exs if int(ex[:2])>=args.start_at]
